d5e4d5ee0e042e670d5daeb0932c7c98d50ad06f
ArmanAghania <Arman94.vergil94@gmail.com>
2023-04-13 23:56:46 +0300

    Authentication Added

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,19 +1,49 @@
 from django.shortcuts import render
 from .models import Notes
-from django.http import Http404
-
-from django.views.generic import DetailView ,ListView
+from django.http import Http404, HttpResponseRedirect
+from .forms import NotesForm
+from django.views.generic import CreateView, DetailView ,ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
 
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
+    
 
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = 'notes'
+
+class NotesCreateView(LoginRequiredMixin, CreateView):
+    model = Notes
+    # fields = ['title', 'text']
+    success_url = '/smart/notes'
+    form_class = NotesForm
+    login_url = '/admin'
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
 
 # def list(request):
 #     all_notes = Notes.objects.all()
--- src/smartnotes/settings.py
+++ src/smartnotes/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = "smartnotes.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'static/templates'
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_REDIRECT_URL = '/smart/notes'
